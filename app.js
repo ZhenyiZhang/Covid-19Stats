2a3c372d34652a88d0d3c92e075068b8040bf20a
Bruce <brucezhangpro@hotmail.com>
2020-03-30 03:44:51 -0400

    cors included

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const waterlooRoute = require('./routes/waterloo');
 const PORT = 5000;
 
 /*bodyParser and cors*/
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
