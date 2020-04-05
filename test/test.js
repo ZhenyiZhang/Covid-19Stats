377591f956a135e5c610c28ab2da2493fbcfe0a5
Bruce <brucezhangpro@hotmail.com>
2020-04-05 01:22:40 -0400

    SMS security

--- test/test.js
+++ test/test.js
@@ -4,24 +4,25 @@ const niagara = require('../cities/niagara');
 const durham = require('../cities/durham');
 const peel = require('../cities/peel');
 const halton = require('../cities/halton');
-require('../.env');
-const client = require('twilio')(process.env.twilioSid,process.env.twilioToken);
+const SID = process.env.twilioSid;
+const token = process.env.twilioToken;
 
-async function test(msgIndicator) {
+async function test() {
     Promise.all([testMsg(waterloo,'waterloo'), testMsg(york,'york'),
         testMsg(niagara, 'niagara'), testMsg(durham, 'durham'), testMsg(peel, 'peel'), testMsg(halton, 'halton')])
         .then(result => {
-            if(msgIndicator) {
+            if(SID && token) {
+                const client = require('twilio')(SID,token);
                 client.messages.create({
                     to: '+19059218478',
                     from: '+12015089195',
                     body: result.toString()
                 }).then(result => console.log(`Test result send by SMS: ${result}`))
                     .catch(err => console.log(err));
-            } else {console.log(result)}
+            }
+            console.log(result);
         });
 }
-
 
 
 function testMsg(test, name) {
