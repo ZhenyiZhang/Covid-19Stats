9f34477b7643ac4785d5965dd3775aa4a0f16db9
Bruce <brucezhangpro@hotmail.com>
2020-04-13 23:45:05 -0400

    york summary added

--- cities/york.js
+++ cities/york.js
@@ -1,5 +1,7 @@
 const url = 'https://ww4.yorkmaps.ca/COVID19/PublicCaseListing/TableListingExternalData.csv';
+const summaryURL = 'https://www.york.ca/covid19';
 const rp = require('request-promise');
+const $ = require('cheerio');
 
 function getYorkData() {
     return new Promise(async (resolve, reject) => {
@@ -22,6 +24,19 @@ function getYorkData() {
             const row = element.split(',');
             table.tbody.push(Array.from(row));
         });
+        table.summary = [];
+        table.summary.push(['total', table.tbody.length]);
+        table.summary.push(['Female', (tableString.match(/Female/g) || []).length]);
+        table.summary.push(['Male', (tableString.match(/Male/g) || []).length]);
+        table.summary.push(['20s', (tableString.match(/20s/g) || []).length]);
+        table.summary.push(['30s', (tableString.match(/30s/g) || []).length]);
+        table.summary.push(['40s', (tableString.match(/40s/g) || []).length]);
+        table.summary.push(['50s', (tableString.match(/50s/g) || []).length]);
+        table.summary.push(['60s', (tableString.match(/60s/g) || []).length]);
+        table.summary.push(['70s', (tableString.match(/70s/g) || []).length]);
+        table.summary.push(['80s', (tableString.match(/80s/g) || []).length]);
+        table.summary.push(['90s', (tableString.match(/90s/g) || []).length]);
+        table.summary.push(['100s', (tableString.match(/100s/g) || []).length]);
         /*add source*/
         table.source = url;
         resolve(table);
